68deaf4becacc38b5e89011390b9a20d98d63589
Alex <alexdev-eclipse@sapo.pt>
2016-11-05 17:34:28 +0000

    Modified Server to handle multiple client connections simultaneously.

--- src/minesweeper/server/MinesweeperServer.java
+++ src/minesweeper/server/MinesweeperServer.java
@@ -4,11 +4,16 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-// Starting Code
+
 /**
  * TODO: thread safety argument goes here!
  */
 public class MinesweeperServer {
+    /**
+     * True is server is listening for connection.
+     */
+    private final boolean listening = true;
+    
     private final ServerSocket serverSocket;
     /**
      * True if the server should _not_ disconnect a client after a BOOM message.
@@ -33,84 +38,13 @@ public class MinesweeperServer {
      *                     (IOExceptions from individual clients do *not* terminate serve())
      */
     public void serve() throws IOException {
-        while (true) {
-            // block until a client connects
-            Socket socket = serverSocket.accept();
-
-            // handle the client
-            try {
-                handleConnection(socket);
-            } catch (IOException e) {
-                e.printStackTrace(); // but don't terminate serve()
-            } finally {
-                socket.close();
-            }
+        while (listening) {
+            // block until a client connects            
+            new MinesweeperServerThread(serverSocket.accept());           
         }
     }
 
-    /**
-     * Handle a single client connection. Returns when client disconnects.
-     * 
-     * @param socket socket where the client is connected
-     * @throws IOException if connection has an error or terminates unexpectedly
-     */
-    private void handleConnection(Socket socket) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-        try {
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-                String output = handleRequest(line);
-                if (output != null) {
-                    out.println(output);
-                }
-            }
-        } finally {
-            out.close();
-            in.close();
-        }
-    }
-
-    /**
-     * Handler for client input, performing requested operations and returning an output message.
-     * 
-     * @param input message from client
-     * @return message to client
-     */
-    private String handleRequest(String input) {
-        String regex = "(look)|(dig -?\\d+ -?\\d+)|(flag -?\\d+ -?\\d+)|"
-                + "(deflag -?\\d+ -?\\d+)|(help)|(bye)";
-        if ( ! input.matches(regex)) {
-            // invalid input
-            return null;
-        }
-        String[] tokens = input.split(" ");
-        if (tokens[0].equals("look")) {
-            // 'look' request
-            // TODO Question 5
-        } else if (tokens[0].equals("help")) {
-            // 'help' request
-            // TODO Question 5
-        } else if (tokens[0].equals("bye")) {
-            // 'bye' request
-            // TODO Question 5
-        } else {
-            int x = Integer.parseInt(tokens[1]);
-            int y = Integer.parseInt(tokens[2]);
-            if (tokens[0].equals("dig")) {
-                // 'dig x y' request
-                // TODO Question 5
-            } else if (tokens[0].equals("flag")) {
-                // 'flag x y' request
-                // TODO Question 5
-            } else if (tokens[0].equals("deflag")) {
-                // 'deflag x y' request
-                // TODO Question 5
-            }
-        }
-        // Should never get here--make sure to return in each of the valid cases above.
-        throw new UnsupportedOperationException();
-    }
+    //was here handleConnection() & hadleRequest(); moved to MinesweeperServerThread ADT
 
     /**
      * Start a MinesweeperServer using the given arguments.
---
+++ src/minesweeper/server/MinesweeperServerThread.java
@@ -0,0 +1,97 @@
+package minesweeper.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MinesweeperServerThread implements Runnable{
+    /**
+     * MSSThread stays for MinesweeperServerThread.
+     */
+    private final Thread MSSThread; // reference to current Thread in case i need to manage it.
+    private final Socket socket;
+    
+    public MinesweeperServerThread(Socket socket){
+        this.socket = socket;
+        MSSThread = new Thread(this);
+        MSSThread.start();
+        
+    }
+    
+    /**
+     * Run a thread
+     */
+    public void run(){
+        try {
+            handleConnection(socket);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    /**
+     * Handle a single client connection. Returns when client disconnects.
+     * 
+     * @param socket socket where the client is connected
+     * @throws IOException if connection has an error or terminates unexpectedly
+     */
+    private void handleConnection(Socket socket) throws IOException {
+        // try-with-resources Statement
+        try(BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            ) 
+        {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String output = handleRequest(line);
+                if (output != null) {
+                    out.println(output);
+                }
+            }
+        }
+    }
+
+    /**
+     * Handler for client input, performing requested operations and returning an output message.
+     * 
+     * @param input message from client
+     * @return message to client
+     */
+    private String handleRequest(String input) {
+        String regex = "(look)|(dig -?\\d+ -?\\d+)|(flag -?\\d+ -?\\d+)|"
+                + "(deflag -?\\d+ -?\\d+)|(help)|(bye)";
+        if ( ! input.matches(regex)) {
+            // invalid input
+            return null;
+        }
+        String[] tokens = input.split(" ");
+        if (tokens[0].equals("look")) {
+            // 'look' request
+            // TODO Question 5
+        } else if (tokens[0].equals("help")) {
+            // 'help' request
+            // TODO Question 5
+        } else if (tokens[0].equals("bye")) {
+            // 'bye' request
+            // TODO Question 5
+        } else {
+            int x = Integer.parseInt(tokens[1]);
+            int y = Integer.parseInt(tokens[2]);
+            if (tokens[0].equals("dig")) {
+                // 'dig x y' request
+                // TODO Question 5
+            } else if (tokens[0].equals("flag")) {
+                // 'flag x y' request
+                // TODO Question 5
+            } else if (tokens[0].equals("deflag")) {
+                // 'deflag x y' request
+                // TODO Question 5
+            }
+        }
+        // Should never get here--make sure to return in each of the valid cases above.
+        throw new UnsupportedOperationException();
+    }
+}
